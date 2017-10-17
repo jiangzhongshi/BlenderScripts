dc42f3afac56c51c49edeb305e7ad8976c76154e
Zhongshi Jiang <jiangzs@nyu.edu>
2017-10-17 17:39:15 -0400

    Update

--- select_some_poses.py
+++ select_some_poses.py
@@ -9,13 +9,15 @@ def random_sample(population, num):
     num = min(num, len(population))
     return random.sample(population, num)
 
-in_path = '/scratch/zj495/Mixamo/'
-out_path = '/scratch/zj495/poses/'
-model_path = os.listdir(in_path)
-for model in model_path:
-    os.makedirs(out_path + model)
-    pose_files = random_sample(glob.glob(in_path + model+'/*.tar.gz'),200 )
-    for pose in pose_files:
-        with tarfile.open(pose, 'r:gz') as tar:
-            tar.extractall(out_path + model ,
-                 random_sample(tar.getmembers(),3))
+if __name__ == '__main__':
+    argv = sys.argv[sys.argv.index("--") + 1:]
+    in_path = argv[0]
+    out_path = argv[1]
+    model_path = os.listdir(in_path)
+    for model in model_path:
+        os.makedirs(out_path + model)
+        pose_files = random_sample(glob.glob(in_path + model+'/*.tar.gz'),20)
+        for pose in pose_files:
+            with tarfile.open(pose, 'r:gz') as tar:
+                tar.extractall(out_path + model ,
+                     random_sample(tar.getmembers(),3))
